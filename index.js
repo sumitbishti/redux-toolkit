228bc1182c0ea1081c5f812957d44e89904be769
Sumit Bisht <sumit7beazt@gmail.com>
2023-04-25 22:11:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const store = require('./app/store')
+const cakeActions = require('./features/cake/cakeSlice').cakeActions
+const icecreamActions = require('./features/icecream/icecreamSlice').icecreamActions
+
+console.log('Intial State ', store.getState())
+
+const unsubscribe = store.subscribe(() => console.log('Updated State ', store.getState()))
+
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.restocked(3))
+
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.restocked(2))
+
+unsubscribe()
