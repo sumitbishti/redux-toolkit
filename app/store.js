52b73a581d3ca316180569e530a3ff626aa2b74c
Sumit Bisht <sumit7beazt@gmail.com>
2023-04-26 21:33:50 +0530

    chnages for async thunks

--- app/store.js
+++ app/store.js
@@ -1,17 +1,19 @@
 const configureStore = require('@reduxjs/toolkit').configureStore
 const cakeReducer = require('../features/cake/cakeSlice')
 const icecreamReducer = require('../features/icecream/icecreamSlice')
-const reduxLogger = require('redux-logger')
+const userReducer = require('../features/user/userSlice')
 
-const logger = reduxLogger.createLogger();
+// const reduxLogger = require('redux-logger')
+// const logger = reduxLogger.createLogger();
 
 
 const store = configureStore({
     reducer: {
         cake: cakeReducer,
-        icecream: icecreamReducer
+        icecream: icecreamReducer,
+        user: userReducer
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
+    // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 })
 
 module.exports = store
--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const store = require('./app/store')
 const cakeActions = require('./features/cake/cakeSlice').cakeActions
 const icecreamActions = require('./features/icecream/icecreamSlice').icecreamActions
+const fetchUsers = require('./features/user/userSlice').fetchUsers
 
 console.log('Intial State ', store.getState())
 
 const unsubscribe = store.subscribe(() => console.log('Updated State ', store.getState()))
 
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.restocked(3))
+store.dispatch(fetchUsers())
 
-store.dispatch(icecreamActions.ordered())
-store.dispatch(icecreamActions.ordered())
-store.dispatch(icecreamActions.restocked(2))
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.restocked(3))
 
-unsubscribe()
+// store.dispatch(icecreamActions.ordered())
+// store.dispatch(icecreamActions.ordered())
+// store.dispatch(icecreamActions.restocked(2))
+
+// unsubscribe()
